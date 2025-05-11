fe6889314edbd8b17c65a9304f9d6332d519541d
Alex-Benoy <20109551@mail.wit.ie>
2025-05-11 17:20:03 +0100

    Addded Cloudfront

--- lib/frontendS3.ts
+++ lib/frontendS3.ts
@@ -2,6 +2,8 @@ import { CfnOutput, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import { Distribution, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class FrontendStack extends Construct {
   constructor(scope: Construct, id: string) {
@@ -20,6 +22,18 @@ export class FrontendStack extends Construct {
     new s3deploy.BucketDeployment(this, "DeplotWebsite", {
       sources: [s3deploy.Source.asset("./dist")],
       destinationBucket: siteBucket,
+    });
+
+    const distribution = new Distribution(this, "SiteDistribution", {
+      defaultBehavior: {
+        origin: new S3Origin(siteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: "index.html",
+    });
+    
+    new CfnOutput(this, "clodfrnt", {
+      value: distribution.distributionDomainName,
     });
 
     new CfnOutput(this, "WebsiteURL", {
